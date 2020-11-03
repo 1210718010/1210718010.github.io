8ccd409e9db0d98afafcc6f526b2e6175d4ea51f
dzb <63325042+1210718010@users.noreply.github.com>
2020-11-03 20:25:48 +0800

    Update svg.js

--- js/svg.js
+++ js/svg.js
@@ -1,12 +1,12 @@
 // JavaScript Document
-var img = "zt.svg";
+var img = "https://k-on.blog/img/zt.svg";
 $(function(){
 	$("#stop").click(function(){
 		img = $(this).attr('src');
-		if (img == "zt.svg") {
-			$("#stop").attr("src", "bf.svg")
+		if (img == "https://k-on.blog/img/zt.svg") {
+			$("#stop").attr("src", "https://k-on.blog/img/bf.svg")
 		}else{
-			$("#stop").attr("src", "zt.svg")
+			$("#stop").attr("src", "https://k-on.blog/img/zt.svg")
 		}
 	});
 });
