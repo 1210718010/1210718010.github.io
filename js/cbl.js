35d1c11c543b1e33d1254f6466ee9c8aeb3152c0
dzb <63325042+1210718010@users.noreply.github.com>
2020-11-04 05:01:08 +0800

    Add files via upload

---
+++ js/cbl.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+$(document).ready(function () {
+	var bbb=$('#bbb');
+	var ccc=$('#ccc');
+	var slider=$('#slider');
+	bbb.click( function () {
+		slider.animate({right:0},500);
+		bbb.attr("class", "gb")
+		ccc.attr("class", "dk")
+	})
+	ccc.click( function () {
+		slider.animate({right:-300},500);
+		ccc.attr("class", "gb")
+		bbb.attr("class", "dk")
+	})
+})
