8b2336dada048e5aae2de668f68b33d3d1cd4a59
dzb <63325042+1210718010@users.noreply.github.com>
2021-06-09 23:53:06 +0800

    Add files via upload

---
+++ js/search.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+function getinput(){
+	var con = document.getElementById('search').value;
+	window.location.href=("https://cn.bing.com/search?q=" + con);
+}
+$("#search").bind("keydown",function(e){
+	let theEvent = e || window.event;
+	let keyCode = theEvent.keyCode || theEvent.which || theEvent.charCode;
+	if (keyCode == 13){
+		getinput();
+	};
+})
