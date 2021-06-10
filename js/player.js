5bc7711b19d50d90fd0fbc00c42d2633eb23aa79
dzb <63325042+1210718010@users.noreply.github.com>
2021-06-10 16:04:43 +0800

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,3 +1,13 @@
-$(function(){
-	document.getElementById("player").innerHTML="<div id=\"bfq\" class=\"divb\"><img id=\"up\" class=\"up1\" alt=\"加\" src=\"https://muxmus.com/img/up.svg\" /><img id=\"down\" class=\"down1\" alt=\"减\" src=\"https://muxmus.com/img/down.svg\" /><a id=\"audio\"><img id=\"stop\" alt=\"播放/暂停\" src=\"https://muxmus.com/img/zt.svg\" /></a><div class=\"divn\"><img id=\"next\" class=\"next1\" alt=\"下一首\" src=\"https://muxmus.com/img/next.svg\" /></div><audio id=\"music\"></audio></div><div class=\"divs\"><p id=\"songs\" class=\"song1\"></p></div>";
-});
+// JavaScript Document
+document.write("<div id=\"bfq\" class=\"divb\">");
+document.write("	<img id=\"up\" class=\"up1\" alt=\"加\" src=\"https:\/\/muxmus.com\/img\/up.svg\" \/>");
+document.write("	<img id=\"down\" class=\"down1\" alt=\"减\" src=\"https:\/\/muxmus.com\/img\/down.svg\" \/>");
+document.write("	<a id=\"audio\"><img id=\"stop\" alt=\"播放\/暂停\" src=\"https:\/\/muxmus.com\/img\/zt.svg\" \/><\/a>");
+document.write("	<div class=\"divn\">");
+document.write("		<img id=\"next\" class=\"next1\" alt=\"下一首\" src=\"https:\/\/muxmus.com\/img\/next.svg\" \/>");
+document.write("	<\/div>");
+document.write("	<audio id=\"music\"><\/audio>");
+document.write("<\/div>");
+document.write("<div class=\"divs\">");
+document.write("	<p id=\"songs\" class=\"song1\"><\/p>");
+document.write("<\/div>");
