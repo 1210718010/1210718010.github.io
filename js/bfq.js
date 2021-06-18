a4c2c55fb9ebfb5ac81045c4ed63c4c272cc6673
dzb <63325042+1210718010@users.noreply.github.com>
2021-06-19 02:23:31 +0800

    Update bfq.js

--- js/bfq.js
+++ js/bfq.js
@@ -7,7 +7,7 @@ document.write("	<a id=\"audio\"><img id=\"stop\" alt=\"播放\/暂停\" src=\"h
 document.write("	<div class=\"divn\">");
 document.write("		<img id=\"next\" class=\"next1\" alt=\"下一首\" src=\"https:\/\/muxmus.com\/img\/next.svg\" \/>");
 document.write("	<\/div>");
-document.write("	<audio id=\"music\"><\/audio>");
+document.write("	<audio id=\"music\" preload=\"auto\"><\/audio>");
 document.write("<\/div>");
 document.write("<div class=\"divs\">");
 document.write("	<p id=\"songs\" class=\"song1\"><\/p>");
