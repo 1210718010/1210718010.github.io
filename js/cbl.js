8a72ea607e141b1c2d513b87a0e3edae6c75ab4d
dzb <63325042+1210718010@users.noreply.github.com>
2021-06-19 06:25:23 +0800

    Update cbl.js

--- js/cbl.js
+++ js/cbl.js
@@ -1,17 +1,17 @@
 // JavaScript Document
 document.write("<div id=\"slider\">");
-document.write("	<img class=\"tx\" src=\"https:\/\/files.catbox.moe\/w8ygtu.png\" alt=\"头像\" \/>");
+document.write("	<img class=\"tx\" src=\"https://files.catbox.moe/w8ygtu.png\" alt=\"头像\" />");
 document.write("	<div class=\"js\">");
-document.write("		<br\/>");
-document.write("		<a href=\"https:\/\/muxmus.com\/\"><p>返回首页<\/p><\/a>");
-document.write("	<\/div>");
-document.write("<\/div>");
+document.write("		<br/>");
+document.write("		<a href=\"https://muxmus.com/\"><p>返回首页</p></a>");
+document.write("	</div>");
+document.write("</div>");
 document.write("<div class=\"dk\" id=\"bbb\">");
-document.write("	<img class=\"cd\" alt=\"打开菜单\" src=\"https:\/\/muxmus.com\/img\/cd.svg\" \/>");
-document.write("<\/div>");
+document.write("	<img class=\"cd\" alt=\"打开菜单\" src=\"https://muxmus.com/img/cd.svg\" />");
+document.write("</div>");
 document.write("<div class=\"gb\" id=\"ccc\">");
-document.write("	<img class=\"cd\" alt=\"关闭菜单\" src=\"https:\/\/muxmus.com\/img\/gb.svg\" \/>");
-document.write("<\/div>");
+document.write("	<img class=\"cd\" alt=\"关闭菜单\" src=\"https://muxmus.com/img/gb.svg\" />");
+document.write("</div>");
 $(document).ready(function () {
 	var bbb=$('#bbb');
 	var ccc=$('#ccc');
