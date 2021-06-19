699b5c55f3925ac5fa09e89199781660ece8da08
dzb <63325042+1210718010@users.noreply.github.com>
2021-06-19 08:08:14 +0800

    Add files via upload

---
+++ js/time.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+function startTime(){
+	var today = new Date();
+	var year = today.getFullYear();
+	var month = today.getMonth() + 1;
+	var day = today.getDate();
+	var hh = today.getHours();
+	var mm = today.getMinutes();
+	var ss = today.getSeconds();
+	month = checkTime(month);
+	day = checkTime(day);
+	hh = checkTime(hh);
+	mm = checkTime(mm);
+	ss = checkTime(ss);
+	document.getElementById('divt').innerHTML = year + "." + month + "." + day + " " + hh + ":" + mm + ":" + ss;
+	setTimeout('startTime()',500);
+}
+function checkTime(i){
+	if(i<10){
+		i = "0" + i;
+	}
+	return i;
+}
