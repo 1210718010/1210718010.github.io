ab65b5d0f10c236abcd167b853ea830b7a11589e
dzb <63325042+1210718010@users.noreply.github.com>
2021-06-24 14:37:06 +0800

    Add files via upload

---
+++ js/link.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+document.body.addEventListener('copy',function(e){
+    if(window.getSelection().toString() && window.getSelection().toString().length > 20){
+        setClipboardText(e);
+    }
+});
+function setClipboardText(event){
+    var clipboardData = event.clipboardData || window.clipboardData;
+    if(clipboardData){
+		event.preventDefault();
+        var htmlData = window.getSelection().toString() + '<br>' + '---------------' + '<br.' + '原文：' + window.location.href;
+        var textData = window.getSelection().toString() + '\n' + '---------------' + '\n' + '原文：' + window.location.href;
+        clipboardData.setData('text/html', htmlData);
+        clipboardData.setData('text/plain',textData);
+    }
+}
