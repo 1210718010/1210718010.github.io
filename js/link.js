c7fd461a3e39b80b9a688b788e0d131eea585f06
dzb <63325042+1210718010@users.noreply.github.com>
2021-06-24 14:52:26 +0800

    Update link.js

--- js/link.js
+++ js/link.js
@@ -8,7 +8,7 @@ function setClipboardText(event){
 	var clipboardData = event.clipboardData || window.clipboardData;
 	if(clipboardData){
 		event.preventDefault();
-		var htmlData = window.getSelection().toString() + '<br>' + '---------------' + '<br.' + '原文：' + window.location.href;
+		var htmlData = window.getSelection().toString() + '<br>' + '---------------' + '<br>' + '原文：' + window.location.href;
 		var textData = window.getSelection().toString() + '\n' + '---------------' + '\n' + '原文：' + window.location.href;
 		clipboardData.setData('text/html', htmlData);
 		clipboardData.setData('text/plain',textData);
