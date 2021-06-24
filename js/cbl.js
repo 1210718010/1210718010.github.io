53e86fdc15837045b4792501ac621fb83ad63e6e
dzb <63325042+1210718010@users.noreply.github.com>
2021-06-24 14:53:47 +0800

    Update cbl.js

--- js/cbl.js
+++ js/cbl.js
@@ -2,7 +2,7 @@
 document.write("<div id=\"slider\">");
 document.write("	<img class=\"tx\" src=\"https://files.catbox.moe/w8ygtu.png\" alt=\"头像\" />");
 document.write("	<div class=\"js\">");
-document.write("		<br/>");
+document.write("		<br>");
 document.write("		<a href=\"https://muxmus.com/\"><p>返回首页</p></a>");
 document.write("	</div>");
 document.write("</div>");
@@ -13,17 +13,17 @@ document.write("<div class=\"gb\" id=\"ccc\">");
 document.write("	<img class=\"cd\" alt=\"关闭菜单\" src=\"https://muxmus.com/img/gb.svg\" />");
 document.write("</div>");
 $(document).ready(function () {
-	var bbb=$('#bbb');
-	var ccc=$('#ccc');
-	var slider=$('#slider');
-	bbb.click( function () {
+	var bbb = $('#bbb');
+	var ccc = $('#ccc');
+	var slider = $('#slider');
+	bbb.click(function(){
 		slider.animate({right:0},500);
-		bbb.attr("class", "gb");
-		ccc.attr("class", "dk");
-	})
-	ccc.click( function () {
+		bbb.attr("class","gb");
+		ccc.attr("class","dk");
+	});
+	ccc.click(function(){
 		slider.animate({right:-350},500);
-		ccc.attr("class", "gb");
-		bbb.attr("class", "dk");
-	})
-})
+		ccc.attr("class","gb");
+		bbb.attr("class","dk");
+	});
+});
