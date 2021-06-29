4f89535a20444bd6c2888ac850462b8849f4dbc9
dzb <63325042+1210718010@users.noreply.github.com>
2021-06-29 16:30:44 +0800

    Update bfq.js

--- js/bfq.js
+++ js/bfq.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-// Update: 2021.06.26 06:38(GMT+8)
+// Update: 2021.06.29 16:30(GMT+8)
 document.write("<div id=\"bfq\" class=\"divb\">");
 document.write("	<img id=\"up\" class=\"up1\" alt=\"加\" src=\"https://muxmus.com/img/up.svg\" />");
 document.write("	<img id=\"down\" class=\"down1\" alt=\"减\" src=\"https://muxmus.com/img/down.svg\" />");
