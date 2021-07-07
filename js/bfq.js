b5e85e218f6640bec58de3870fc7e3c02b3d389f
dzb <63325042+1210718010@users.noreply.github.com>
2021-07-07 19:13:45 +0800

    Update bfq.js

--- js/bfq.js
+++ js/bfq.js
@@ -1,11 +1,11 @@
 // JavaScript Document
 // Update: 2021.07.06 12:40(GMT+8)
 document.write("<div id=\"bfq\" class=\"divb\">");
-document.write("	<img id=\"up\" class=\"up1\" alt=\"加\" src=\"https://muxmus.com/img/up.svg\" />");
-document.write("	<img id=\"down\" class=\"down1\" alt=\"减\" src=\"https://muxmus.com/img/down.svg\" />");
-document.write("	<a id=\"audio\"><img id=\"stop\" alt=\"播放/暂停\" src=\"https://muxmus.com/img/zt.svg\" /></a>");
+document.write("	<img id=\"up\" class=\"up1\" alt=\"\" src=\"https://muxmus.com/img/up.svg\" />");
+document.write("	<img id=\"down\" class=\"down1\" alt=\"\" src=\"https://muxmus.com/img/down.svg\" />");
+document.write("	<a id=\"audio\"><img id=\"stop\" alt=\"\" src=\"https://muxmus.com/img/zt.svg\" /></a>");
 document.write("	<div class=\"divn\">");
-document.write("		<img id=\"next\" class=\"next1\" alt=\"下一首\" src=\"https://muxmus.com/img/next.svg\" />");
+document.write("		<img id=\"next\" class=\"next1\" alt=\"\" src=\"https://muxmus.com/img/next.svg\" />");
 document.write("	</div>");
 document.write("	<audio id=\"music\" preload=\"auto\"></audio>");
 document.write("</div>");
