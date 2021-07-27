1efffde1467307dc8f1b47a17f03126f545f157b
dzb <63325042+1210718010@users.noreply.github.com>
2021-07-28 03:22:54 +0800

    Update cbl.js

--- js/cbl.js
+++ js/cbl.js
@@ -16,7 +16,7 @@ tips[12] = "</div>";
 tips[13] = "<div class=\"gb\" id=\"ccc\">";
 tips[14] = "	<img class=\"cd\" alt=\"\" src=\"https://muxmus.com/img/gb.svg\" />";
 tips[15] = "</div>";
-for (var i = 0;i < tips.length;i++){ 
+for (var i = 0; i < tips.length; i++){ 
 	document.getElementById('cbl').innerHTML = tips[i];
 }
 $(document).ready(function(){
