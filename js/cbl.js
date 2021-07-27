d96d4fc8feb5ed38358b977cfba110e29f980fc2
dzb <63325042+1210718010@users.noreply.github.com>
2021-07-28 03:08:09 +0800

    Update cbl.js

--- js/cbl.js
+++ js/cbl.js
@@ -1,20 +1,22 @@
 // JavaScript Document
-document.write("<div id=\"slider\">");
-document.write("	<img class=\"tx\" src=\"https://files.catbox.moe/w8ygtu.png\" alt=\"\" />");
-document.write("	<div class=\"js\">");
-document.write("		<br>");
-document.write("		<a date-pjax href=\"https://muxmus.com/blog/\"><p>返回首页</p></a>");
-document.write("		<br>");
-document.write("		<a date-pjax href=\"https://muxmus.com/blog/20201225/\"><p>关于《全员恶玉》的一些看法</p></a>");
-document.write("		<a date-pjax href=\"https://muxmus.com/blog/20210601/\"><p>关于原神游戏适龄提示的一些讨论</p></a>");
-document.write("	</div>");
-document.write("</div>");
-document.write("<div class=\"dk\" id=\"bbb\">");
-document.write("	<img class=\"cd\" alt=\"\" src=\"https://muxmus.com/img/cd.svg\" />");
-document.write("</div>");
-document.write("<div class=\"gb\" id=\"ccc\">");
-document.write("	<img class=\"cd\" alt=\"\" src=\"https://muxmus.com/img/gb.svg\" />");
-document.write("</div>");
+var tips = new Array;
+tips[0] = "<div id=\"slider\">";
+tips[1] = "	<img class=\"tx\" src=\"https://files.catbox.moe/w8ygtu.png\" alt=\"\" />";
+tips[2] = "	<div class=\"js\">";
+tips[3] = "		<br>";
+tips[4] = "		<a date-pjax href=\"https://muxmus.com/blog/\"><p>返回首页</p></a>";
+tips[5] = "		<br>";
+tips[6] = "		<a date-pjax href=\"https://muxmus.com/blog/20201225/\"><p>关于《全员恶玉》的一些看法</p></a>";
+tips[7] = "		<a date-pjax href=\"https://muxmus.com/blog/20210601/\"><p>关于原神游戏适龄提示的一些讨论</p></a>";
+tips[8] = "	</div>";
+tips[9] = "</div>";
+tips[10] = "<div class=\"dk\" id=\"bbb\">";
+tips[11] = "	<img class=\"cd\" alt=\"\" src=\"https://muxmus.com/img/cd.svg\" />";
+tips[12] = "</div>";
+tips[13] = "<div class=\"gb\" id=\"ccc\">";
+tips[14] = "	<img class=\"cd\" alt=\"\" src=\"https://muxmus.com/img/gb.svg\" />";
+tips[15] = "</div>";
+document.getElementById('cbl').innerHTML=(tips);
 $(document).ready(function(){
 	var bbb = $('#bbb');
 	var ccc = $('#ccc');
