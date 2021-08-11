e3dee77655e4e21ef15a0a40400cbeced7f0920e
dzb <63325042+1210718010@users.noreply.github.com>
2021-08-12 07:38:49 +0800

    Update 1.js

--- js/bfq/1.js
+++ js/bfq/1.js
@@ -1,10 +1,12 @@
 // JavaScript Document
 if ('mediaSession' in navigator) {
 	navigator.mediaSession.metadata = new MediaMetadata({
-	title: '123',
-	artist: '321',
+	title: '木辛木杉',
 	artwork: [
-	{src: 'https://files.catbox.moe/mnl4p1.jpg', sizes: '1920x1080', type: 'image/jpge'}
+		{src: 'https://files.catbox.moe/mnl4p1.jpg', sizes: '1920x1080', type: 'image/jpge'}
 	]
 	});
+	navigator.mediaSession.setActionHandler('play', porp());
+	navigator.mediaSession.setActionHandler('pause', porp());
+	navigator.mediaSession.setActionHandler('nexttrack', clickNext());
 }
