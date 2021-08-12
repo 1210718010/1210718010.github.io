f27c299d731bd6d132f61265f2bea391b8fbe9b7
dzb <63325042+1210718010@users.noreply.github.com>
2021-08-12 08:06:02 +0800

    Update 1.js

--- js/bfq/1.js
+++ js/bfq/1.js
@@ -2,9 +2,17 @@
 if ('mediaSession' in navigator) {
 	navigator.mediaSession.metadata = new MediaMetadata({
 		title: '木辛木杉',
-		artwork: [
-			{src: 'https://files.catbox.moe/mnl4p1.jpg', sizes: '1920x1080', type: 'image/jpge'}
-		]
+		artwork: [{src: "https://files.catbox.moe/mnl4p1.jpg"}]
+	});
+	navigator.mediaSession.setActionHandler('play', function(){
+		music.play();
+		navigator.mediaSession.playbackState = "playing";
+		$("#stop").attr("src", "https://muxmus.com/img/bf.svg");
+	});
+	navigator.mediaSession.setActionHandler('pause', function(){
+		music.pause();
+		navigator.mediaSession.playbackState = "Paused";
+		$("#stop").attr("src", "https://muxmus.com/img/zt.svg");
 	});
 	navigator.mediaSession.setActionHandler('nexttrack', function(){
 		nextSong();
