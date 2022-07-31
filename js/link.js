f37353c151742f217fa37dbdffc0b9bfe8bf0145
dzb <63325042+1210718010@users.noreply.github.com>
2022-07-31 16:38:17 +0800

    Update link.js

--- js/link.js
+++ js/link.js
@@ -8,8 +8,8 @@ function setClipboardText(event){
 	var clipboardData = event.clipboardData || window.clipboardData;
 	if(clipboardData){
 		event.preventDefault();
-		var htmlData = window.getSelection().toString() + '<br>' + '<br>' + '--------------------' + '<br>' + '作者：木辛木杉' + '<br>' + '原文：' + window.location.href;
-		var textData = window.getSelection().toString() + '\n' + '\n' + '--------------------' + '\n' + '作者：木辛木杉' + '\n' + '原文：' + window.location.href;
+		var htmlData = window.getSelection().toString() + '<br>' + '<br>' + '--------------------' + '<br>' + '作者：木辛木杉' + '<br>' + '原文：' + window.location.href + '<br>' + 'MIT License' + '<br>' + 'Copyright (c) 2022 木辛木杉';
+		var textData = window.getSelection().toString() + '\n' + '\n' + '--------------------' + '\n' + '作者：木辛木杉' + '\n' + '原文：' + window.location.href + '\n' + 'MIT License'+ '\n' + 'Copyright (c) 2022 木辛木杉';
 		clipboardData.setData('text/html', htmlData);
 		clipboardData.setData('text/plain',textData);
 	}
