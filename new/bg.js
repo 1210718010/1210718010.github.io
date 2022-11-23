e15b32f921dc2408b73644e58036e84b2de54d19
dzb <1210718010@qq.com>
2022-11-23 20:33:20 +0800

    Update bg.js

--- new/bg.js
+++ new/bg.js
@@ -168,7 +168,7 @@ function autoSwitch(){
 	document.body.style.backgroundPosition = "center";
 	bgI += 1;
 	img1 = new Image();
-	img1.src = "//pximg.rainchan.win/img?img_id=" + bg[bgOriginal[bgI + 1]].id;
+	img1.src = "//pximg.rainchan.win/img?img_id=" + bg[bgOriginal[bgI + 1]].id ;
 }
 function ifSwitch(){
 	if(bgOriginal[0] == bgNum){
