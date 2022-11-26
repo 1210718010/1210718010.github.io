7d879a4ed8a5ee552852b50f05e8da3a18e6d921
dzb <1210718010@qq.com>
2022-11-26 14:22:36 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,20 +1,20 @@
 // JavaScript Document
 document.write("<div id=\"divt\"></div>");
-setInterval(startTime(),100);
 function startTime(){
-	var today = new Date();
-	var year = today.getFullYear();
-	var month = today.getMonth() + 1;
-	var day = today.getDate();
-	var hh = today.getHours();
-	var mm = today.getMinutes();
-	var ss = today.getSeconds();
+	var now = new Date();
+	var year = now.getFullYear();
+	var month = now.getMonth() + 1;
+	var day = now.getDate();
+	var hh = now.getHours();
+	var mm = now.getMinutes();
+	var ss = now.getSeconds();
 	month = checkTime(month);
 	day = checkTime(day);
 	hh = checkTime(hh);
 	mm = checkTime(mm);
 	ss = checkTime(ss);
 	document.getElementById('divt').innerHTML = year + "." + month + "." + day + " " + hh + ":" + mm + ":" + ss;
+	setTime(startTime(),100);
 }
 function checkTime(i){
 	if(i<10){
