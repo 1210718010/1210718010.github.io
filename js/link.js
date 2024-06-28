fbd65604689b94389e47b344414d576452d7e9e2
木辛木杉 <1210718010@qq.com>
2024-06-29 03:22:51 +0800

    Update link.js

--- js/link.js
+++ js/link.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 document.body.addEventListener('copy',function(e){
-	if(window.getSelection().toString() && window.getSelection().toString().length > 20){
+	if(window.getSelection().toString() && window.getSelection().toString().length > 30){
 		setClipboardText(e);
 	}
 });
@@ -8,8 +8,8 @@ function setClipboardText(event){
 	var clipboardData = event.clipboardData || window.clipboardData;
 	if(clipboardData){
 		event.preventDefault();
-		var htmlData = window.getSelection().toString() + '<br>' + '<br>' + '--------------------' + '<br>' + '作者：木辛木杉' + '<br>' + '原文：' + window.location.href + '<br>' + 'MIT License' + '<br>' + 'Copyright (c) 2019-2023 木辛木杉';
-		var textData = window.getSelection().toString() + '\n' + '\n' + '--------------------' + '\n' + '作者：木辛木杉' + '\n' + '原文：' + window.location.href + '\n' + 'MIT License'+ '\n' + 'Copyright (c) 2019-2023 木辛木杉';
+		var htmlData = window.getSelection().toString() + '<br>' + '<br>' + '--------------------' + '<br>' + '作者：木辛木杉' + '<br>' + '原文：' + window.location.href + '<br>' + 'MIT License' + '<br>' + 'Copyright (c) 2019-2024 木辛木杉';
+		var textData = window.getSelection().toString() + '\n' + '\n' + '--------------------' + '\n' + '作者：木辛木杉' + '\n' + '原文：' + window.location.href + '\n' + 'MIT License'+ '\n' + 'Copyright (c) 2019-2024 木辛木杉';
 		clipboardData.setData('text/html', htmlData);
 		clipboardData.setData('text/plain',textData);
 	}
