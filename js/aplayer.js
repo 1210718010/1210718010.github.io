b9d149e393ef99e1b801b63df770d8748954359c
木辛木杉 <1210718010@qq.com>
2024-06-29 03:10:23 +0800

    Add files via upload

---
+++ js/aplayer.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+$.ajax({
+	type:"get",
+	url:"https://file.muxmus.com:5000/api/music",
+	dataType:"json",
+	success:function(jsonData){
+		const ap = new APlayer({
+			container: document.getElementById("aplayer"),
+			fixed: true,
+			theme: "#8E8CD8",
+			order: "random",
+			volume: 0.3,
+			lrcType: 3,
+			audio: jsonData,
+		});
+		ap.on('play', () => {
+			const currentPlayMeta = ap.list.audios[ap.list.index];
+			navigator.mediaSession.metadata = new MediaMetadata({
+				title: currentPlayMeta.name,
+				artist: currentPlayMeta.artist,
+				album: currentPlayMeta.artist,
+				artwork: [{ src: currentPlayMeta.cover || '' }],
+			});
+			navigator.mediaSession.setActionHandler('previoustrack',function(){
+				ap.skipBack();
+			});
+			navigator.mediaSession.setActionHandler('nexttrack',function(){
+				ap.skipForward();
+			});
+		});
+	},
+});
