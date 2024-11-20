ddcfa33cfff0fcb21783418c1c118a420c20dafa
木辛木杉 <1210718010@qq.com>
2024-11-20 09:18:38 +0800

    Update pjax.js

--- js/pjax.js
+++ js/pjax.js
@@ -2,6 +2,19 @@ $(document).pjax('a[date-pjax]', '#main-content', {
         fragment: '#main-content', timeout: '60000'
         });
 $(document).on('ready pjax:end', function(event) {
+        var bbb = $("#bbb");
+	var ccc = $("#ccc");
+	var slider = $("#slider");
+	bbb.click(function(){
+		slider.css({'right':'0px','opacity':'1'});
+		bbb.attr("class","gb");
+		ccc.attr("class","dk");
+        });
+	ccc.click(function(){
+		slider.css({'right':'-400px','opacity':'0'});
+		ccc.attr("class","gb");
+		bbb.attr("class","dk");
+        });
         hljs.highlightAll();
         twikoo.init({
                 envId: 'https://twikoo.muxmus.com',
