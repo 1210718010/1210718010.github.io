0001a577ea9b2ae5b769407625a10b84bc65f235
木辛木杉 <1210718010@qq.com>
2024-11-20 09:14:12 +0800

    Update pjax.js

--- js/pjax.js
+++ js/pjax.js
@@ -1,12 +1,12 @@
 $(document).pjax('a[date-pjax]', '#main-content', {
-    fragment: '#main-content', timeout: '60000'
-    });
+        fragment: '#main-content', timeout: '60000'
+        });
 $(document).on('ready pjax:end', function(event) {
-    hljs.highlightAll();
-    twikoo.init({
-        envId: 'https://twikoo.muxmus.com',
-        el: '#tcomment',
-        lang: 'zh-CN'
-        })
-    });
+        hljs.highlightAll();
+        twikoo.init({
+                envId: 'https://twikoo.muxmus.com',
+                el: '#tcomment',
+                lang: 'zh-CN'
+                })
+        });
 hljs.highlightAll();
