334190e5ba5b1345edd583eaec4b1404e37009e1
木辛木杉 <1210718010@qq.com>
2024-11-20 11:47:28 +0800

    Update pjax.js

--- js/pjax.js
+++ js/pjax.js
@@ -5,7 +5,7 @@ $(document).on('ready pjax:end', function(event) {
 	var sliderHref = window.location.href;
 	$("#slider-link a").each(function(){
 		if($(this).attr('href') == sliderHref){
-			$(this).css({'pointer-events':'none','opacity':'.7','background-color':'#000','border-radius':'5px','padding':'2px'})
+			$(this).css({'opacity':'.7','background-color':'#000','border-radius':'5px','padding':'2px','cursor':'not-allowed','text-decoration':'none'})
 		}else{
 			$(this).removeAttr("style","")
 		}
